bfb1be1a8d194de3302044a2b34efb9373cfe74c
kauntak <n.koshirae+github@gmail.com>
2021-12-27 06:22:25 -0700

    Update homeAnimation.js

--- scripts/homeAnimation.js
+++ scripts/homeAnimation.js
@@ -2,11 +2,13 @@ var nameSentenceElement = document.getElementById("name-sentence");//.addEventLi
 var nickNameSentenceElement = document.getElementById("nick-name-sentence");//.addEventListener("animationstart", changeFullNameToNickName(), false);
 var titleSentenceElement = document.getElementById("title-sentence");//.addEventListener("animationstart", typeTitle(), false);
 var smallScreenInfo = document.getElementById("small-screen-info");
+// var contactSentenceElement = document.getElementById("contact-sentence");
 
+nameSentenceElement.onanimationstart = typeFullName;
+nickNameSentenceElement.onanimationstart = changeFullNameToNickName;
+titleSentenceElement.onanimationstart = typeTitle;
+// contactSentenceElement.onanimationend = changeHome();
 
-nameSentenceElement.onanimationend = typeFullName;
-nickNameSentenceElement.onanimationend = changeFullNameToNickName;
-titleSentenceElement.onanimationend = typeTitle;
 let fullName = document.getElementById("full-name").innerHTML;
 let nickName = document.getElementById("nick-name").innerHTML;
 var typeWriterSpeed = 50;
@@ -37,6 +39,9 @@ function typeNickname(){
     }
 }
 function typeFullName(){
+    setTimeout(typeFullName2, 1250);
+}
+function typeFullName2(){
     i = 0;
     text = fullName;
     id = "name-to-edit";
@@ -45,6 +50,9 @@ function typeFullName(){
 }
 
 function changeFullNameToNickName(){
+    setTimeout(fullNameToNickName, 1250);
+}
+function fullNameToNickName(){
     i = 0;
     while(i < nickName.length && i < fullName.length){
         if(fullName[i] != nickName[i])
@@ -59,12 +67,15 @@ function changeFullNameToNickName(){
 }
 
 function typeTitle(){
+    setTimeout(typeTitle2, 1250);
+}
+function typeTitle2(){
     text = document.getElementById("title").innerHTML;
     id = "title-to-edit";
     i = 0;
     document.getElementById(id).innerHTML = "";
     addText();
-    setTimeout(condenseIn, 2000);
+    setTimeout(changeHome, 9000);
 }
 
 function animateSkillOrb(){
@@ -72,7 +83,7 @@ function animateSkillOrb(){
         var timeout = 2000;
     else
         timeout = 15000;
-    setTimeout(condenseIn, 2000);
+    setTimeout(changeHome, 8000);
 }
 function condenseIn(){
     let id = null;
@@ -81,7 +92,7 @@ function condenseIn(){
     let scale = 2;
     clearInterval(id);
     id = setInterval(frame, 5);
-
+    // setTimeout(changeHome, 9000);
     function frame(){
         if(scale <= 1 && opacity >= 1 ){
             clearInterval(id);
@@ -96,4 +107,29 @@ function condenseIn(){
             }
         }
     }
+}
+
+function changeHome(){
+    setTimeout(changeLeft, 1000);
+    setTimeout(changeRight, 1000);
+}
+
+function changeLeft(){
+    var leftDiv = document.getElementById("home-left");
+    leftDiv.style.opacity = 0;
+    leftDiv.style.width = 0;
+}
+function changeRight(){
+    const rightDiv = document.getElementById("home-right");
+    rightDiv.style.width = "80vw";
+    // const profileInfo = document.getElementById("large-screen-info");
+    // profileInfo.style.position = "relative";
+    // profileInfo.style.left = "10%";
+    // profileInfo.style.top = "5%";
+    
+    const globe = document.getElementById("myCanvasContainer");
+    globe.style.position = "absolute";
+    globe.style.left = "0";
+	globe.style.top = "20vh";
+    condenseIn();
 }
